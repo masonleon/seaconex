dc45c7b861d152bb7af62818a656a0d94eb6f2b3
Mason Leon <44509637+masonleon@users.noreply.github.com>
2021-03-23 16:26:37 -0400

    add map.js

---
+++ js/map.js
@@ -0,0 +1,53 @@
+var width = 960,
+        height = 500;
+    
+    var projection = d3.geoMercator()
+        .center([0, 0])
+        .scale(200)
+        .rotate([0,0]);
+    
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+    
+    var path = d3.geoPath()
+        .projection(projection);
+    
+    var g = svg.append("g");
+    
+    // load and display the World
+    d3.json("countries-110m.json").then(function(topology) {
+    
+      // load and display the cities
+      d3.json('./data/processed/terminal.json').then(function(data) {
+          g.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function(d) {
+                    return projection([d.terminal_lon, d.terminal_lat])[0];
+            })
+            .attr("cy", function(d) {
+                    return projection([d.terminal_lon, d.terminal_lat])[1];
+            })
+            .attr("r", 5)
+            .style("fill", "red");
+      });
+
+      g.selectAll("path")
+          .data(topojson.feature(topology, topology.objects.countries).features)
+          .enter().append("path")
+          .attr("d", path);
+    
+    });
+
+    var zoom = d3.zoom()
+      .scaleExtent([1, 20])
+      .on('zoom', function(event) {
+          g.selectAll('path')
+           .attr('transform', event.transform);
+          g.selectAll("circle")
+           .attr('transform', event.transform);
+});
+
+svg.call(zoom);
