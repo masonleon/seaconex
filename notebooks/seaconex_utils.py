6aefb579277475fafb15db28ba7dacc1b5a9a394
Mason Leon <44509637+masonleon@users.noreply.github.com>
2021-03-25 14:16:17 -0400

    add geometry util

---
+++ notebooks/seaconex_utils.py
@@ -0,0 +1,20 @@
+"""
+Converting Degrees, Minutes, Seconds formatted coordinate strings to decimal.
+
+Formula:
+DEC = (DEG + (MIN * 1/60) + (SEC * 1/60 * 1/60))
+
+Assumes S/W are negative.
+"""
+
+import re
+
+#https://www.javaer101.com/en/article/18832510.html
+#https://stackoverflow.com/questions/33997361
+def dms2dd(s):
+    # example: s = """0°51'56.29"S"""
+    degrees, minutes, seconds, direction = re.split('[°\'"]+', s)
+    dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60);
+    if direction in ('S','W'):
+        dd*= -1
+    return dd
