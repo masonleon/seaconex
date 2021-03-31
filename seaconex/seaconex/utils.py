ed78ce317ad4b0ea1fe0cac04d61bea6d2da7f39
Mason Leon <44509637+masonleon@users.noreply.github.com>
2021-03-31 13:05:51 -0400

    cleanup style

--- seaconex/seaconex/utils.py
+++ seaconex/seaconex/utils.py
@@ -18,66 +18,67 @@ Assumes S/W are negative.
 
 
 def dms_to_dd(s):
-    # example: s = """0°51'56.29"S"""
-    degrees, minutes, seconds, direction = re.split('[°\'"]+', s)
-    dd = float(degrees) + float(minutes) / 60 + float(seconds) / (60 * 60)
-    if direction in ('S', 'W'):
-        dd *= -1
-    return dd
+  # example: s = """0°51'56.29"S"""
+  degrees, minutes, seconds, direction = re.split('[°\'"]+', s)
+  dd = float(degrees) + float(minutes) / 60 + float(seconds) / (60 * 60)
+  if direction in ('S', 'W'):
+    dd *= -1
+  return dd
 
 
 def piped_txt_to_arr(file):
-    data = []
+  data = []
 
-    with open(file, 'rt') as in_file:
-        for line in in_file:
-            if line[0] == '|':
-                cells = [x.strip() for x in line[1:len(line) - 2].split('|')]
-                data.append(cells)
-    return data
+  with open(file, 'rt') as in_file:
+    for line in in_file:
+      if line[0] == '|':
+        cells = [x.strip() for x in line[1:len(line) - 2].split('|')]
+        data.append(cells)
+  return data
+
 
 # /api/publications/world-port-index
 # World Port Index Database Queries
 def nga_msi_wpi_api_get_world_port_index(port_codes):
-    url = 'https://msi.nga.mil/api/publications/world-port-index?'
+  url = 'https://msi.nga.mil/api/publications/world-port-index?'
 
-    ports = []
+  ports = []
 
-    for port in port_codes:
-        params = {
-            "indexNumber": port,
-            "output": "json"
-        }
+  for port in port_codes:
+    params = {
+      "indexNumber": port,
+      "output": "json"
+    }
 
-        try:
-            r = requests.get(url, params=params)
+    try:
+      r = requests.get(url, params=params)
 
-            if r.status_code == 200:
-                ports.append(r.json()['ports'][0])
-        except:
-            print('error portNumber: {}'.format(port))
+      if r.status_code == 200:
+        ports.append(r.json()['ports'][0])
+    except:
+      print('error portNumber: {}'.format(port))
 
-    data = {'ports': ports}
-    return data
+  data = {'ports': ports}
+  return data
 
 
 # /api/publications/world-port-index/wpi-prc-names
 # List Port, Region, and Country Names for World Port Indexes
 def nga_msi_wpi_api_get_prc_names():
-    url = 'https://msi.nga.mil/api/publications/world-port-index/wpi-prc-names'
+  url = 'https://msi.nga.mil/api/publications/world-port-index/wpi-prc-names'
 
-    data = {}
+  data = {}
 
-    params = {
-        "output": "json"
-    }
+  params = {
+    "output": "json"
+  }
 
-    try:
-        r = requests.get(url, params=params)
+  try:
+    r = requests.get(url, params=params)
 
-        if r.status_code == 200:
-            data = r.json()
-    except:
-        print('error')
+    if r.status_code == 200:
+      data = r.json()
+  except:
+    print('error')
 
-    return data
+  return data
