07aa5ae5b43a5e579a7e2f3418726e5072e10c87
Alice Tilles <tilles.al@husky.neu.edu>
2021-04-05 13:04:08 -0400

    HTML shell for filters

--- js/visualization.js
+++ js/visualization.js
@@ -13,10 +13,25 @@
   let visMap1 = svgMap()
   ('#vis-map-1', topology, terminals, edges);
 
+  function getMasterSchedule(data) {
+
+    // console.log("data.features:", data.features);
+    // let originTerminals = data.features
+    //   .filter(feature => feature.id === 'master_schedule')
+    //   .map((feature, i) => feature.properties.source_terminal)
+    //   .filter ((item, i, ar) => ar.indexOf(item) === i)
+
+    // console.log("Filtered:", originTerminals)
+
+  }
+
+
   d3.json(trajectory).then(data => {
 
     let visMap2 = leafletMap()
     ('#vis-map-2', data);
+
+    // getMasterSchedule(data)
 
   })
 
