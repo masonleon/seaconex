bc140aa52dbb724d2388058e4d2ad9894c757444
Mason Leon <44509637+masonleon@users.noreply.github.com>
2021-04-08 20:40:12 -0400

    carrierFilter brushing

--- js/visualization.js
+++ js/visualization.js
@@ -16,9 +16,6 @@
   const masterSchedulesTerminalCallInfo = './data/interim/master_schedules_terminal_call_info.geojson'
   const trajectory = './data/interim/timestamped-trajectory-icl-tac1.geojson'
 
-  // let geoData = d3.merge([
-  //     []
-  // ])
   Promise.all([
     d3.json(terminals),
     d3.json(masterSchedulesEdges),
@@ -41,29 +38,53 @@
       // 'timestamped_trajectory': data[6]
     }
 
-
-    // console.log(data[0])  // first row of cities
-    // console.log(d)  // first row of animals
-    // d3.merge([data]).then(value => console.log(value));
-    // console.log();
   }).then(data => {
     console.log(data)
 
-    let visControls = filterControls()
-    ('#filters-component', data)
+    // General event type for selections, used by d3-dispatch
+    // https://github.com/d3/d3-dispatch
+    const dispatchString = 'selectionUpdated';
+
+    let visControls = carrierFilter()
+      .selectionDispatcher(d3.dispatch(dispatchString))
+      ('#filters-component', data)
 
     let nodeViz = network()
-    ('#vis-network', data);
+      ('#vis-network', data);
 
     let visMap1 = svgMap()
-    // //  ('#vis-map-1', topology, terminals, dataset);
-    ('#vis-map-1', data);
-    //
-    // let visMap2 = leafletMap()
-    // ('#vis-map-2', data);
+      ('#vis-map-1', data);
 
-    let visDetails = detailPane()
-    ('#detail-pane', data);
+    let visMap2 = leafletMap()
+      ('#vis-map-2', data);
+
+    // let visDetails = detailPane()
+    //   ('#detail-pane', data);
+
+    let charts = [
+      visControls,
+      // nodeViz,
+      // visMap1
+    ];
+
+    // When any chart selection is updated via brushing,
+    // have all other charts to update their selections (linking)
+    for (let i = 0; i < charts.length; i++) {
+      for (let j = 0; j < charts.length; j++) {
+        if (i === j) continue;
+
+        // We can't just use the same string for them all. In order to register
+        // multiple event handlers (e.g., multiple charts listening for your selection
+        // change) we need to have 'prefix.suffix' format where the suffix is arbitrary
+        // but unique.
+        let pairUniqueString = dispatchString + '.' + i + '.' + j;
+
+        // Where we actually tell one chart to listen to the other.
+        charts[i]
+          .selectionDispatcher()
+          .on(pairUniqueString, charts[j].updateSelection);
+      }
+    }
   });
 
 
